395809206b9ecd1f37cf42de3f46f59276a01244
Elvia Choi <elviachoi924@gmail.com>
2025-09-16 00:39:42 +0900

    fix: 찜한 아이돌 3개 초과 시 목록 미노출 오류 수정

--- src/pages/idolSearch/IdolSearchList.tsx
+++ src/pages/idolSearch/IdolSearchList.tsx
@@ -1,7 +1,5 @@
-import { VirtuosoGrid } from "react-virtuoso";
 import Card from "../../components/common/card";
 import type { Idol } from "./types";
-import GridFooter from "../../components/GridFooter";
 import { useFavoriteStore } from "../../store/favorites";
 
 type Props = {
@@ -16,49 +14,30 @@ type Props = {
 
 export default function IdolSearchList({
   idols,
-  isSearching,
-  hasNextPage,
-  fetchNextPage,
-  isFetchingNextPage,
   onCardClick,
   toggleFavorite,
 }: Props) {
   const { favoriteIds } = useFavoriteStore();
 
-  const renderItem = (_: number, idol: Idol) => (
-    <div className='flex items-center justify-center p-2'>
-      <Card
-        type='idol'
-        idolId={Number(idol.id)}
-        title={idol.name}
-        imageSrc={idol.avatarUrl || ""}
-        isFavorite={favoriteIds.includes(idol.id)}
-        detail={{
-          idolGroup: idol.groupName ?? "",
-          position: idol.position ?? "",
-        }}
-        onClick={() => onCardClick(Number(idol.id))}
-        toggleFavorite={toggleFavorite}
-      />
-    </div>
-  );
-
   return (
-    <VirtuosoGrid
-      useWindowScroll
-      data={idols}
-      computeItemKey={(_, idol) => idol.id}
-      endReached={() => {
-        if (isSearching && hasNextPage && !isFetchingNextPage) {
-          fetchNextPage();
-        }
-      }}
-      increaseViewportBy={isSearching ? { top: 0, bottom: 100 } : undefined}
-      overscan={2}
-      components={{ Footer: GridFooter }}
-      context={{ isFetchingNextPage }}
-      listClassName='grid grid-cols-1 gap-x-2 gap-y-6 sm:grid-cols-2 md:gap-y-8 lg:grid-cols-3 lg:gap-y-10'
-      itemContent={renderItem}
-    />
+    <div className='grid grid-cols-1 gap-x-2 gap-y-6 sm:grid-cols-2 md:gap-y-8 lg:grid-cols-3 lg:gap-y-10'>
+      {idols.map((idol) => (
+        <div key={idol.id} className='flex items-center justify-center p-2'>
+          <Card
+            type='idol'
+            idolId={Number(idol.id)}
+            title={idol.name}
+            imageSrc={idol.avatarUrl || ""}
+            isFavorite={favoriteIds.includes(idol.id)}
+            detail={{
+              idolGroup: idol.groupName ?? "",
+              position: idol.position ?? "",
+            }}
+            onClick={() => onCardClick(Number(idol.id))}
+            toggleFavorite={toggleFavorite}
+          />
+        </div>
+      ))}
+    </div>
   );
 }
