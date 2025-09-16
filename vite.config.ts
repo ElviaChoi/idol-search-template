3e6eebb236939949c5acd7c793eb82e3e66c867c
Elvia Choi <elviachoi924@gmail.com>
2025-09-17 02:53:39 +0900

    🔧 fix: Vercel 빌드 오류 해결
    - vite.config.ts에서 __dirname 대신 import.meta.url을 사용하도록 수정
    
    - Node.js 타입 확인을 위해 @types/node 의존성 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { fileURLToPath, URL } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
 });
